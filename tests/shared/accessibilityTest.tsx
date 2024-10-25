6906c514999b2dd98d11ea6e7d86698d1b147b3e
小豪 <1844749591@qq.com>
2024-10-26 01:14:55 +0800

    refactor(tests): relocate setup configuration to individual test file for better isolation

--- tests/shared/accessibilityTest.tsx
+++ tests/shared/accessibilityTest.tsx
@@ -3,17 +3,71 @@ import { render } from '@testing-library/react';
 import { globSync } from 'glob';
 import { axe } from 'jest-axe';
 
+// Create a queue to manage axe calls
+let axeRunning = false;
+const runAxe = async (...args: Parameters<typeof axe>) => {
+  // eslint-disable-next-line no-unmodified-loop-condition
+  while (axeRunning) {
+    await new Promise((resolve) => setTimeout(resolve, 100));
+  }
+  axeRunning = true;
+  try {
+    return await axe(...args);
+  } finally {
+    axeRunning = false;
+  }
+};
+
 // eslint-disable-next-line jest/no-export
 export default function accessibilityTest(Component: React.ComponentType) {
+  beforeAll(() => {
+    // Fake ResizeObserver
+    global.ResizeObserver = jest.fn(() => {
+      return {
+        observe() {},
+        unobserve() {},
+        disconnect() {},
+      };
+    }) as jest.Mock;
+
+    // fake fetch
+    global.fetch = jest.fn(() => {
+      return {
+        then() {
+          return this;
+        },
+        catch() {
+          return this;
+        },
+        finally() {
+          return this;
+        },
+      };
+    }) as jest.Mock;
+  });
+
+  beforeEach(() => {
+    // Reset all mocks
+    if (global.fetch) {
+      (global.fetch as jest.Mock).mockClear();
+    }
+  });
+
+  afterEach(() => {
+    // Clear all mocks
+    jest.clearAllMocks();
+  });
+
   describe(`accessibility`, () => {
     it(`component does not have any violations`, async () => {
       jest.useRealTimers();
       const { container } = render(<Component />);
-      const results = await axe(container, {
+      const results = await runAxe(container, {
         rules: {
           'image-alt': { enabled: false },
           label: { enabled: false },
           'button-name': { enabled: false },
+          'role-img-alt': { enabled: false },
         },
       });
       expect(results).toHaveNoViolations();
@@ -37,7 +91,10 @@ export function accessibilityDemoTest(component: string, options: Options = {}) 
 
   describe(`${component} demo a11y`, () => {
     const files = globSync(`./components/${component}/demo/*.tsx`).filter(
-      (file) => !file.includes('_semantic') && !file.includes('-debug'),
+      (file) =>
+        !file.includes('_semantic') &&
+        !file.includes('-debug') &&
+        !file.includes('component-token'),
     );
 
     files.forEach((file) => {
